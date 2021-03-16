214cd6599472bdca0173d2a7912e98962721faea
amigma00 <ali.aliaman786@gmail.com>
2021-03-16 16:12:21 +0530

    list is not scrolling in main

---
+++ lib/Lists.dart
@@ -0,0 +1,40 @@
+List image = [
+  "resources/gg.jpg",
+  "resources/bb.jpg",
+  "resources/cc.png",
+  "resources/dd.jpg",
+  "resources/ee.png",
+  "resources/ff.png"
+];
+List rname = [
+  "Raghu Franky",
+  "LittiWale",
+  "Jail chai Bar",
+  "Amdavadi DalVada Centre",
+  "Gupta Chat Centre",
+  "The Mothers Kitchen",
+];
+List category = [
+  "Fast Food Corners",
+  "Fast Food Corners",
+  "Fast Food Corners",
+  "Fast Food Corners",
+  "Fast Food Corners",
+  "Fast Food Corners",
+];
+List dtime = [
+  "Delivery in 35 mins",
+  "Delivery in 35 mins",
+  "Delivery in 35 mins",
+  "Delivery in 35 mins",
+  "Delivery in 35 mins",
+  "Delivery in 35 mins",
+];
+List off = [
+  "Upto 5% off",
+  "Upto 5% off",
+  "Upto 5% off",
+  "Upto 5% off",
+  "Upto 5% off",
+  "Upto 5% off",
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Lists.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -54,116 +56,157 @@ class _MyHomePageState extends State<MyHomePage> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
 
-        children: <Widget>[
-          SizedBox(
-            height: 10,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  //SizedBox(width: 10),
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    //SizedBox(width: 10),
 
-                  Container(
-                    child: FlatButton(
-                        textColor: Colors.white,
-                        color: Colors.deepOrange,
-                        child: Text('All'),
-                        onPressed: () {},
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    child: FlatButton(
-                        textColor: Colors.white,
-                        color: Colors.deepOrange,
-                        child: Text('  Delivery Time  '),
-                        onPressed: () {},
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    child: FlatButton(
-                        textColor: Colors.white,
-                        color: Colors.deepOrange,
-                        child: Text('  Top Offer  '),
-                        onPressed: () {},
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    child: FlatButton(
-                        textColor: Colors.white,
-                        color: Colors.deepOrange,
-                        child: Text('  Top Offer  '),
-                        onPressed: () {},
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                ],
+                    Container(
+                      child: FlatButton(
+                          textColor: Colors.white,
+                          color: Colors.deepOrange,
+                          child: Text('All'),
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0))),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      child: FlatButton(
+                          textColor: Colors.white,
+                          color: Colors.transparent,
+                          highlightColor: Colors.deepOrange,
+                          child: Text('  Delivery Time  '),
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Colors.deepOrange,
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0))),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      child: FlatButton(
+                          textColor: Colors.white,
+                          color: Colors.transparent,
+                          highlightColor: Colors.deepOrange,
+                          child: Text('  Top Offer  '),
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Colors.deepOrange,
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0))),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      child: FlatButton(
+                          textColor: Colors.white,
+                          color: Colors.transparent,
+                          highlightColor: Colors.deepOrange,
+                          child: Text('  Top Offer  '),
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Colors.deepOrange,
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0))),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(7),
-                  child: Image.asset(
-                    'resources/db.jpg',
-                    scale: 5,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  height: 100,
-                  width: 200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Raghu Franky',
-                          style: TextStyle(
-
-                              fontSize: 18, fontWeight: FontWeight.bold)),
-                      Text('Fast Food Corners',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          )),
-                      Row(
-                        children: [
-                          Icon(Icons.delivery_dining,color: Colors.deepOrange),
-                          SizedBox(width: 5,),
-                          Text('Delivery in 35 mins',style: TextStyle(color: Colors.grey,),)
-                        ],
-                      ),Row(
-                        children: [
-                          Icon(Icons.local_offer_outlined,color: Colors.deepOrange),
-                          SizedBox(width: 5,),
-                          Text('Up to 13% off')
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(child: Icon(Icons.star,color: Colors.deepOrange))
-              ],
+            SizedBox(
+              height: 5,
             ),
-          ),
-        ],
+            ListView.builder(
+                shrinkWrap: true,
+                //scrollDirection: Axis.horizontal,
+                itemCount: rname.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+
+                    margin: EdgeInsets.all(5),
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(7),
+                          child: Image.asset(
+                            image[index],
+                            scale: 1,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          height: 100,
+                          width: 215,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(rname[index],
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold)),
+                              Text(category[index],
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey,
+                                  )),
+                              Row(
+                                children: [
+                                  Icon(Icons.delivery_dining,
+                                      color: Colors.deepOrange),
+                                  SizedBox(
+                                    width: 2,
+                                  ),
+                                  Text(
+                                    dtime[index],
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.local_offer_outlined,
+                                      color: Colors.deepOrange),
+                                  SizedBox(
+                                    width: 2,
+                                  ),
+                                  Text(off[index])
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                            child: Icon(Icons.star, color: Colors.deepOrange))
+                      ],
+                    ),
+                  );
+                }),
+          ],
+        ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
